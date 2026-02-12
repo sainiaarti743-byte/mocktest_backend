7e780fca408df9e1d69c33f2be10338e6ac4af2e
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-02-12 10:50:14 +0530

    up

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,30 +3,35 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.adminLogin = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    const user = await User.findOne({ email, role: 'admin' });
-    if (!user) return res.status(404).json({ message: "Admin not found" });
+        const user = await User.findOne({ email, role: 'admin' });
+        if (!user) return res.status(404).json({ message: "Admin not found" });
 
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) return res.status(401).json({ message: "Wrong password" });
 
-    const token = jwt.sign(
-      { id: user._id, role: user.role },
-      "ADMIN_SECRET_KEY",
-      { expiresIn: "1d" }
-    );
 
-    res.json({
-      message: "Admin login success",
-      token,
-      admin: user
-    });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Wrong password" });
+        }
 
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            "ADMIN_SECRET_KEY",
+            { expiresIn: "1d" }
+        );
+
+        res.json({
+            message: "Admin login success",
+            token,
+            admin: user
+        });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 
