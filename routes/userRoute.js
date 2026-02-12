ea287d61f6ee20e73af96f4aa139db2940e93f4e
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-02-12 10:16:53 +0530

    up

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -13,27 +13,27 @@
 
 const express = require('express');
 const router = express.Router();
-// const {adminLogin} = require('../controllers/userController');
+const {adminLogin} = require('../controllers/userController');
 
-// router.post('/admin/login',adminLogin);
-
-
+router.post('/admin/login',adminLogin);
 
 
-router.get('/create-admin', async (req, res) => {
-    const bcrypt = require('bcryptjs')
 
-    const User = require('../models/user');
-    const hash = await bcrypt.hash("admin123", 10);
 
-    const admin = await User.create({
-        name: "Super Admin",
-        email: 'admin@gmail.com',
-        password: hash,
-        role: 'admin'
-    });
-    res.json(admin)
-})
+// router.get('/create-admin', async (req, res) => {
+//     const bcrypt = require('bcryptjs')
+
+//     const User = require('../models/user');
+//     const hash = await bcrypt.hash("admin123", 10);
+
+//     const admin = await User.create({
+//         name: "Super Admin",
+//         email: 'admin@gmail.com',
+//         password: hash,
+//         role: 'admin'
+//     });
+//     res.json(admin)
+// })
 
 
 module.exports = router;
