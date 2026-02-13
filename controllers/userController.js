34805b3aac214191a9203258bee16535a8b45ba8
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-02-13 09:50:54 +0530

    up

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,39 +1,32 @@
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
+
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
 
-exports.adminLogin = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+exports.adminLogin = async (req,res)=>{
+  const {email,password} = req.body;
 
-        const user = await User.findOne({ email, role: 'admin' });
-        if (!user) return res.status(404).json({ message: "Admin not found" });
+  const user = await User.findOne({email, role:'admin'});
+  if(!user) return res.status(404).json({message:"Admin not found"});
 
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch) return res.status(401).json({message:"Wrong password"});
 
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn:'1d' }
+  );
 
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ message: "Wrong password" });
-        }
-
-
-        const token = jwt.sign(
-            { id: user._id, role: user.role },
-            "ADMIN_SECRET_KEY",
-            { expiresIn: "1d" }
-        );
-
-        res.json({
-            message: "Admin login success",
-            token,
-            admin: user
-        });
-
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+  res.json({
+    message:"Login success",
+    token,
+    admin:{
+      id:user._id,
+      name:user.name,
+      email:user.email
     }
+  });
 };
-
-
 
 
--- routes/mocktest.routes.js
+++ routes/mocktest.routes.js
@@ -7,13 +7,16 @@ const { createMockTest
     deleteMockTest
 
 } = require('../controllers/mocktest.controller');
+const {verifyToken} = require('../middlewares/auth');
 
-router.post('/create', createMockTest);
-router.get('/Mocklist', getMockTests);
-router.get('/:id', getSingleMockTest);
-router.put('/:id', updateMockTest);
-router.delete('/:id', deleteMockTest);
+router.post('/create',verifyToken, createMockTest);
+router.get('/Mocklist',verifyToken, getMockTests);
+router.get('/:id',verifyToken, getSingleMockTest);
+router.put('/update/:id',verifyToken, updateMockTest);
+router.delete('/delete/:id',verifyToken, deleteMockTest);
 
 
 module.exports = router;
+
+
 
